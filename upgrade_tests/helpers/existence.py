32562e28fa38f9ff891b2e6e5591dc6f0d2cca4e
ntkathole <nikhilkathole2683@gmail.com>
2018-12-04 14:42:30 +0530

    Add 6.5 postupgrade supported version for existence tests

--- upgrade_tests/helpers/existence.py
+++ upgrade_tests/helpers/existence.py
@@ -303,7 +303,7 @@ def compare_postupgrade(component, attribute):
     :returns tuple: The tuple containing two items, first attribute value
         before upgrade and second attribute value of post upgrade
     """
-    sat_vers = ['6.1', '6.2', '6.3', '6.4']
+    sat_vers = ['6.1', '6.2', '6.3', '6.4', '6.5']
     from_ver = os.environ.get('FROM_VERSION')
     to_ver = os.environ.get('TO_VERSION')
     endpoint = os.environ.get('ENDPOINT')
--- upgrade_tests/helpers/variants.py
+++ upgrade_tests/helpers/variants.py
@@ -204,7 +204,7 @@ def assert_varients(component, pre, post):
         Else compares the actual preupgrade and postupgrade values and returns
         True/False accordingly
     """
-    supported_versions = ['6.1', '6.2', '6.3', '6.4']
+    supported_versions = ['6.1', '6.2', '6.3', '6.4', '6.5']
     from_version = os.environ.get('FROM_VERSION')
     to_version = os.environ.get('TO_VERSION')
 

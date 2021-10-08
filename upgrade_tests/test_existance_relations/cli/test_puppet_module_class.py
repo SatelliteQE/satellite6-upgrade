265c0a6ebb957ad1caee998ff95f5ed74b6ab2f7
Tomas Strych <tomas.strych2@gmail.com>
2021-10-08 22:49:03 +0200

    remove puppet module

--- upgrade_tests/test_existance_relations/cli/test_puppet_module_class.py
+++ upgrade_tests/test_existance_relations/cli/test_puppet_module_class.py
@@ -26,8 +26,6 @@ from upgrade_tests.helpers.existence import pytest_ids
 # Required Data
 component_class = 'puppet-class'
 pc_name = compare_postupgrade(component_class, 'name')
-component_module = 'puppet-module'
-pm_name = compare_postupgrade(component_module, 'name')
 
 
 # Tests
@@ -38,18 +36,6 @@ def test_positive_puppet_classes_by_name(pre, post):
     :id: upgrade-44e7617c-4092-42bd-9b28-907c034966f7
 
     :expectedresults: All puppet classes should be retained post upgrade by
-        names
-    """
-    assert existence(pre, post)
-
-
-@pytest.mark.parametrize("pre,post", pm_name, ids=pytest_ids(pm_name))
-def test_positive_puppet_modules_by_name(pre, post):
-    """Test all puppet modules are existing after upgrade by names
-
-    :id: upgrade-9749e8e0-8424-4825-9350-5475413eba8e
-
-    :expectedresults: All puppet modules should be retained post upgrade by
         names
     """
     assert existence(pre, post)

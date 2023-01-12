238ec855fc9492e96a42ca432df846ab2fd506fd
Lukas Pramuk <lpramuk@redhat.com>
2023-01-12 13:00:40 +0100

    Remove fixed/wontfix BZs
    (cherry picked from commit 2b19a0061237281cbd31a443d7d4ad297e4d7f93)

--- upgrade_tests/test_existance_relations/cli/test_gpg.py
+++ upgrade_tests/test_existance_relations/cli/test_gpg.py
@@ -18,7 +18,6 @@
 """
 import pytest
 
-from upgrade_tests.helpers.common import dont_run_to_upgrade
 from upgrade_tests.helpers.common import existence
 from upgrade_tests.helpers.existence import compare_postupgrade
 from upgrade_tests.helpers.existence import pytest_ids
@@ -30,7 +29,6 @@ gpg_name = compare_postupgrade(component, 'name')
 # Tests
 
 
-@dont_run_to_upgrade(['6.8', '6.9', '6.10', '6.11'])
 @pytest.mark.parametrize("pre,post", gpg_name, ids=pytest_ids(gpg_name))
 def test_positive_gpg_keys_by_name(pre, post):
     """Test all gpg keys are existing after upgrade by names
